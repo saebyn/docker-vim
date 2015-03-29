05de13ef6414d51e61a66aaf108647ac2b5739b3
John Weaver <john@saebyn.info>
2015-03-29 11:37:47 -0700

    Updates lein deps.

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -3,7 +3,7 @@
                   [lein-typed "0.3.5"]
                   [jonase/eastwood "0.1.4"]]
         :dependencies [[clj-stacktrace "0.2.7"]
-                       [spyscope "0.1.3"]
+                       [spyscope "0.1.5"]
                        [redl "0.2.4"]]
         :injections [(require 'spyscope.core)
                      (require '[redl complete core])
